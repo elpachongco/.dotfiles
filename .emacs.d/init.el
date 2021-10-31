bcd432e1faa55fe3c46ae5697a8ffe51af0eaeb1
elpachongco <elpachongco@gmail.com>
2021-10-31 17:00:48 +0800

    Changed author

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,20 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-safe-themes
-   '("6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default))
- '(package-selected-packages
-   '(company smartparens smart-parens ace-window which-key try use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(ansi-color-faces-vector
+;; 	[default default default italic underline success warning error])
+;;  '(custom-safe-themes
+;; 	'("6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default))
+;;  '(package-selected-packages
+;; 	'(helpful company smartparens smart-parens ace-window which-key try use-package)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 (setq inhibit-startup-message t)
 
@@ -76,6 +76,9 @@
   :ensure t
   :init)
 (smartparens-global-mode 1)
+(show-smartparens-global-mode 1)
+
+;; Prettify symbols
 (global-prettify-symbols-mode 1)
 
 ;; Comp any
@@ -86,19 +89,14 @@
 
 (scroll-bar-mode -1)
 
-(setq tab-width 3)
-
-
 ;; Theme
-(use-package gruvbox-theme
+(use-package gruber-darker-theme
   :ensure t  :init)
-(load-theme 'gruvbox-dark-hard t)
-(set-background-color "black")
-;;(add-to-list 'default-frame-alist '(background-color . "black")) 
+(load-theme 'gruber-darker t)
 
 ;; Set font for all frames
-(set-frame-font "IBM Plex Mono-11:hinting=false:hintstyle=hintfull\
-:autohint=false:antialias=true" :frames t)
+(set-frame-font "IBM Plex Mono-11:hinting=true:hintstyle=hintfull\
+:autohint=false:antialias=false" :frames t)
 
 ;; Set column rule
 (column-number-mode 1)
@@ -106,3 +104,18 @@
 (global-display-fill-column-indicator-mode 80)
 (display-fill-column-indicator-mode 80)
 
+;; Set indents to tab
+(setq indent-tabs-mode 1)
+(setq-default tab-width 3)
+
+(use-package helpful
+  :ensure t)
+
+(global-set-key (kbd "C-h f") #'helpful-callable)
+;; (global-set-key (kbd "C-h f") #'helpful-function)
+(global-set-key (kbd "C-h v") #'helpful-variable)
+(global-set-key (kbd "C-h k") #'helpful-key)
+
+;; Set help buffers to display in current buffer
+(add-to-list 'display-buffer-alist
+             '("*Help*" display-buffer-same-window))
