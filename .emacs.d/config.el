a3e332d662d65a50b2343c8acab93e017bca7cdc
elpachongco <earlsiachongco@gmail.com>
2021-11-04 11:47:25 +0800

    added go mode

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -84,6 +84,8 @@
 
 (setq use-dialog-box nil)
 
+(global-auto-revert-mode t)
+
 ;; Smartparens
 	 (use-package smartparens
 	 :ensure t
@@ -114,7 +116,7 @@
 (column-number-mode 1)
 (setq-default fill-column 80)
 ;; Enable automatically on prog mode
-(add-hook 'prog-mode-hook (display-fill-column-indicator-mode 80)) 
+(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode) 
 ;;(global-display-fill-column-indicator-mode 80)
 ;;(display-fill-column-indicator-mode 80)
 
@@ -159,34 +161,10 @@
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
-(use-package tide
-  :ensure t)
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;; For editing TSX files
-  (require 'web-mode)
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  (add-hook 'web-mode-hook
-     (lambda ()
-(when (string-equal "tsx" (file-name-extension buffer-file-name))
-(setup-tide-mode))))
-	  ;; enable typescript-tslint checker
-	  (flycheck-add-mode 'typescript-tslint 'web-mode)
-
 (add-hook 'python-mode-hook 'eglot-ensure)
+
+(use-package go-mode
+:ensure t)
 
 ;; Allow <s shortcuts
 (require 'org-tempo)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,10 +12,11 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/.dotfiles/.emacs.d/config.org" "~/Personal/todo.org"))
  '(package-selected-packages
-	'(evil web-mode use-package try tide smartparens highlight-parentheses helpful gruvbox-theme gruber-darker-theme eglot company ace-window)))
+	'(go-mode evil web-mode use-package try tide smartparens highlight-parentheses helpful gruvbox-theme gruber-darker-theme eglot company ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(whitespace-tab ((t (:foreground "#636363")))))
